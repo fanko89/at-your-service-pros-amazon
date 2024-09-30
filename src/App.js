893d082ba026f0cc9e9a0285b433ba69cd6663c6
fanko89 <deek311@gmail.com>
2024-09-30 15:07:41 -0600

    logo link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header d-flex align-items-center justify-content-center">
+      <a href="https://www.atyourservicepros.us" target="_blank" rel="noopener noreferrer">
         <img src={logo} alt="At Your Service Pros" className="logo" />
+        </a>
         <h2 className="ms-3">At Your Service Pros </h2><h5>(Approved Products)</h5>
       </header>
       <main>
